e53f1cb84428255f5afc51cb82ee0096908707a7
Elagoht <furkanbaytekin@gmail.com>
2021-12-31 19:05:59 +0300

    Ready to Release

---
+++ README.md
@@ -0,0 +1,10 @@
+# i3-startup-apps
+
+This script lets you to locate your startup apps into ~/.config/autostart/
+directory. Just place the .desktop files here and run the script. A config file
+will be create on your i3 config directory. Then you must include that file on
+your i3 config file.
+
+Simply **add this line** to your config file:
+
+> include ~/.config/i3/startup-apps
---
+++ refresh-autostart.py
@@ -0,0 +1,10 @@
+#!/bin/env python3
+from os import listdir, popen
+directory=popen("echo $HOME").read().strip()+"/.config/"
+files=listdir(directory+"autostart/")
+result=""
+for filename in files:
+    with open(directory+"autostart/"+filename,"r",encoding="UTF-8") as file:
+        for line in file.readlines():
+            if line[:5]=="Exec=": result+="exec "+line[5:]
+with open(directory+"i3/startup-apps","w") as file: file.write(result)
